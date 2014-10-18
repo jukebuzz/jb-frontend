a7237f7df459a5de5da91f5e5af087aa53cbc866
Sergey Ponomarev <sergey.ponomarev@saltpepper.ru>
2014-10-18 11:51:01 +0400

    Add csrf protection to API endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,10 @@ class ApplicationController < ActionController::Base
   include Authenticatable
   include ErrorHandler
 
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session unless Rails.env.development?
+  after_filter :set_csrf_cookie
+
+  def set_csrf_cookie
+    cookies['CSRF-Token'] = form_authenticity_token if protect_against_forgery?
+  end
 end
