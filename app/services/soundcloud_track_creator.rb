ab1a5603e21d05a3148ce6e0f3cbd4d0d08ecfde
Vladimir Lyzo <donbobka@gmail.com>
2014-10-18 22:33:13 +0400

    :scissors: puts

--- app/services/soundcloud_track_creator.rb
+++ app/services/soundcloud_track_creator.rb
@@ -2,10 +2,8 @@ class SoundcloudTrackCreator < Service
   attr_reader :soundcloud_id
 
   def call
-    p soundcloud_id
     Track.find_or_create_by(soundcloud_id: soundcloud_id) do |track|
       soundcloud_res = Soundcloud.current.get("/tracks/#{soundcloud_id}")
-      p soundcloud_res
 
       track.artwork_url = soundcloud_res.artwork_url
       track.stream_url = soundcloud_res.stream_url
