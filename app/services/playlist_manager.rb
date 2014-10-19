18e66880cbddf1882436fd1547d7c1b5bc546b20
Vladimir Lyzo <donbobka@gmail.com>
2014-10-19 17:02:42 +0400

    Add coin spending

--- app/services/playlist_manager.rb
+++ app/services/playlist_manager.rb
@@ -2,14 +2,34 @@ class PlaylistManager < Service
   attr_reader :playlist, :user
 
   def append_soundcloud_track(soundcloud_id)
-    (track = soundcloud_track(soundcloud_id)).persisted? && playlist.append_track(track, user)
+    Coins::Spender.new(user: user, room: playlist.room).call(Settings.coins.pricing_plan.append) do
+      (track = soundcloud_track(soundcloud_id)).persisted? && playlist.append_track(track, user)
+    end
   end
 
   def prepend_soundcloud_track(soundcloud_id)
-    (track = soundcloud_track(soundcloud_id)).persisted? && playlist.prepend_track(track, user)
+    Coins::Spender.new(user: user, room: playlist.room).call(Settings.coins.pricing_plan.prepend) do
+      (track = soundcloud_track(soundcloud_id)).persisted? && playlist.prepend_track(track, user)
+    end
   end
 
-  delegate :delete_track, :move_up_track, :move_down_track, to: :playlist
+  def delete_track(*args)
+    Coins::Spender.new(user: user, room: playlist.room).call(Settings.coins.pricing_plan.delete) do
+      playlist.delete_track(*args)
+    end
+  end
+
+  def move_up_track(*args)
+    Coins::Spender.new(user: user, room: playlist.room).call(Settings.coins.pricing_plan.move_up) do
+      playlist.move_up_track(*args)
+    end
+  end
+
+  def move_down_track(*args)
+    Coins::Spender.new(user: user, room: playlist.room).call(Settings.coins.pricing_plan.move_down) do
+      playlist.move_down_track(*args)
+    end
+  end
 
   private
 
